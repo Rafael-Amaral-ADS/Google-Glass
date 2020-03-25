21a8b067ee70306ecbbac2dabb13ee51d681f048
Rafael Amaral <rafael.amaral.carvalho@hotmail.com>
2020-03-25 10:40:01 -0300

    Projetos Python

---
+++ venv/Teste_Vetor.py
@@ -0,0 +1,11 @@
+N = int(input('Quantos números você vai digitar? '))
+
+vet: [float] = [0 for x in range(N)]
+
+for i in range(0, N):
+    vet[i] = float(input('Digite um número: '))
+
+print()
+print('NÚMEROS DIGITADOS: ')
+for i in range(0, N):
+    print(f'{vet[i]:.1f}')
